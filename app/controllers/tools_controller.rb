6fd70d6de052d06fbf8ae3503f3faa0019850568
ina-gaki <ina.p1192@gmail.com>
2022-06-01 19:34:28 +0900

    コントローラー修正

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,17 @@
 class ToolsController < ApplicationController
   def index
   end
+
+  def create
+    reship = Reship.create(reship_params)
+    render json:{ reship: reship }
+  end
+
+
+  private
+
+  def reship_params
+    params.permit(:flour,:water,:salt,:sugar,:yeast,:oil)
+  end
+
 end
