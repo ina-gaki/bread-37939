98a7dc36c576de1eb5578d12de34a50d8ba8720b
ina-gaki <ina.p1192@gmail.com>
2022-06-02 12:16:32 +0900

    リファレンス

--- app/javascript/tool.js
+++ app/javascript/tool.js
@@ -1,3 +1,29 @@
+const buildHTML = (XHR) => {
+  const bakers_water = (water.value) / (flour.value) * 100;
+  const bakers_salt = (salt.value) / (flour.value) * 100;
+  const bakers_sugar = (sugar.value) / (flour.value) * 100;
+  const bakers_oil = (oil.value) / (flour.value) * 100;
+  const bakers_yeast = (yeast.value) / (flour.value) * 100;
+
+  const item = XHR.response.reship;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        小麦粉：${item.flour}g
+      </div>
+      <div class="post-content">
+        小麦粉:100%
+        水:${bakers_water.toFixed(0)}%
+        塩:${bakers_salt.toFixed(0)}%
+        砂糖:${bakers_sugar.toFixed(0)}%
+        イースト:${bakers_yeast.toFixed(0)}%
+        油脂:${bakers_oil.toFixed(0)}%
+
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -10,7 +36,6 @@ function post (){
     XHR.send(formData);
     XHR.onload = () => {
       const list = document.getElementById("list");
-
       const flour = document.getElementById("flour");
       const water = document.getElementById("water");
       const salt = document.getElementById("salt");
@@ -18,30 +43,7 @@ function post (){
       const oil = document.getElementById("oil");
       const yeast = document.getElementById("yeast");
 
-      const bakers_water = (water.value) / (flour.value) * 100;
-      const bakers_salt = (salt.value) / (flour.value) * 100;
-      const bakers_sugar = (sugar.value) / (flour.value) * 100;
-      const bakers_oil = (oil.value) / (flour.value) * 100;
-      const bakers_yeast = (yeast.value) / (flour.value) * 100;
-
-
-      const item = XHR.response.reship;
-      const html = `
-        <div class="post">
-          <div class="post-date">
-            小麦粉：${item.flour}g
-          </div>
-          <div class="post-content">
-            小麦粉:100%
-            水:${bakers_water.toFixed(0)}%
-            塩:${bakers_salt.toFixed(0)}%
-            砂糖:${bakers_sugar.toFixed(0)}%
-            イースト:${bakers_yeast.toFixed(0)}%
-            油脂:${bakers_oil.toFixed(0)}%
-
-          </div>
-        </div>`;
-      list.innerHTML = html;
+      list.innerHTML = buildHTML(XHR);
     };
   });
 };
