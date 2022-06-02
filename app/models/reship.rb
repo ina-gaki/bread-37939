0aeb8950da3662d81880f66e84a88f15dd2596c5
ina-gaki <ina.p1192@gmail.com>
2022-06-03 08:28:07 +0900

    バリデーション追加

--- app/models/reship.rb
+++ app/models/reship.rb
@@ -1,2 +1,10 @@
 class Reship < ApplicationRecord
+  with_options presence: true , numericality: { only_integer: true } do
+    validates :flour
+    validates :water
+    validates :salt
+    validates :sugar
+    validates :yeast
+    validates :oil
+  end
 end
--- app/models/reship2.rb
+++ app/models/reship2.rb
@@ -1,2 +1,12 @@
 class Reship2 < ApplicationRecord
+  with_options presence: true , numericality: { only_integer: true } do
+    validates :flour_g
+    validates :flour2_1
+    validates :flour2_2
+    validates :water2
+    validates :salt2
+    validates :sugar2
+    validates :yeast2
+    validates :oil2
+  end
 end
