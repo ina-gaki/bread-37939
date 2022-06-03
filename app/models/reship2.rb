5bdf9f0221fdff9a61639d04eb9fc587d633c92f
ina-gaki <ina.p1192@gmail.com>
2022-06-03 13:07:07 +0900

    Update reship2.rb

--- app/models/reship2.rb
+++ app/models/reship2.rb
@@ -1,17 +1,12 @@
 class Reship2 < ApplicationRecord
   with_options presence: true , numericality: { only_integer: true } do
     validates :flour_g
-    validates :flour2_1 , :flour_calculation
-    validates :flour2_2 , :flour_calculation
+    validates :flour2_1
+    validates :flour2_2
     validates :water2
     validates :salt2
     validates :sugar2
     validates :yeast2
     validates :oil2
   end
-end
-
-def flour_calculation
-    if flour2_1 + flour2_2 != 100
-    end
 end
